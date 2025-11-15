8915ced330dc9a8e12f567801f9bc022705b62c6
Lorenzo Garcet <mrgarcet@live.com>
2025-11-14 23:47:34 -0500

    Adding old missing legacy link_cleaner.py

---
+++ legacy/scrapper/legacy_link_cleaner.py
@@ -0,0 +1,46 @@
+import os
+import ast
+
+def clean_links(url):
+    # Find the index of the "#" character
+    index = url.find('#')
+    
+    # If "#" is found, remove everything after it
+    if index != -1:
+        url = url[:index]
+    
+    return url
+
+def create_list_without_duplicates(lst):
+    cleaned_links_set = []
+
+    for i in lst:
+        if i not in cleaned_links_set:
+            cleaned_links_set.append(i)
+
+    return cleaned_links_set
+
+def append_list_to_file(lst, filename):
+    try:
+        # Open the file in append mode
+        with open(filename, 'a') as file:
+            # Write each item from the list to the file
+            for item in lst:
+                file.write(str(item) + '\n')
+        print("List contents appended to", filename)
+    except Exception as e:
+        print("Error:", e)
+
+
+cleaned_links_list = []
+
+file = open("processed_mdc_links.txt", 'r')
+link_contents = file.read()
+link_list = ast.literal_eval(link_contents)
+file.close()
+
+
+for n in range(0,len(link_list)):
+    cleaned_links_list.append(clean_links(link_list[n]))
+
+append_list_to_file(create_list_without_duplicates(cleaned_links_list),"cleaned_links_set.txt")
