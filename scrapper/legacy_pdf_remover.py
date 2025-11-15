b167180e76ee9df169b61062856322366a572127
Lorenzo Garcet <mrgarcet@live.com>
2025-11-14 22:15:11 -0500

    Updated naming convention to distinguish between legacy and new pipeline

--- scrapper/legacy_pdf_remover.py
+++ scrapper/legacy_pdf_remover.py
@@ -1,6 +1,6 @@
 import ast
 
-with open ("../data/cleaned_links_set.txt", 'r') as file:
+with open ("../data/legacy_cleaned_links_set.txt", 'r') as file:
     list_contents = file.read()
     links_list = ast.literal_eval(list_contents)
 
@@ -10,7 +10,7 @@ for i in links_list:
               new_clean.append(i)
 print(len(new_clean))
 
-with open("../data/no_pdf_list.txt", 'w') as file:
+with open("../data/legacy_no_pdf_list.txt", 'w') as file:
       no_pdf_list = str(new_clean)
       file.write(no_pdf_list)
 
